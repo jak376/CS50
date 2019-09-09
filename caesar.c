bbc79f2423e59be87744b2bbb866878bb6632ccf
jak376 <54381417+jak376@users.noreply.github.com>
2019-09-08 21:25:12 -0400

    Update caesar.c

--- caesar.c
+++ caesar.c
@@ -1,13 +1,17 @@
-#include <cs50.h>
+=#include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 int main(int argc, string argv[])
 {
-    if (argc==2 || argv[2] == int)
-    {
-        printf("Success\n");
-    }   
+    if (argc==2)
+    for (int i = 0; i < strlen(argv[1]); i++)
+        {
+            if (isdigit(argv[i]))    
+            printf("Success\n");
+        }   
     else
     {
         printf("Usage ./caesar key\n");
