6858afa3c274fdc008197d63ae5a38c15fde1de1
jak376 <54381417+jak376@users.noreply.github.com>
2019-09-08 21:10:21 -0400

    Add files via upload
    ask user for command line argument
    make sure are characters in argument are digits
    convert the string from the argument to an integer
    ask user for plaintext
    for each character in text
      convert each character to an integer
      if integer corresponds with lowercase letter, rotae and keep lowercase
      if integer corresponds to uppercase letter, rotate and keep case
      else, print character as is
    print new line

---
+++ caesar.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    if (argc==2 || argv[2] == int)
+    {
+        printf("Success\n");
+    }   
+    else
+    {
+        printf("Usage ./caesar key\n");
+    }
+}
